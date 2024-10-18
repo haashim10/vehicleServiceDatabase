592116b76931ece8a524d2f1f208e02def524b17
haashim10 <91482390+haashim10@users.noreply.github.com>
2024-10-18 15:24:56 +0100

    Add files via upload

---
+++ Pages/Vehicles/Create.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using trans_track.Data;
+using trans_track.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Amazon.S3;
+using Amazon;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
+
+namespace trans_track.Pages.Vehicle
+{
+    public class CreateModel : PageModel
+    {
+        private readonly trans_track.Data.ApplicationDbContext _context;
+        private readonly ICommon _common;
+        
+        public CreateModel(trans_track.Data.ApplicationDbContext context, ICommon common)
+        {
+            _context = context;
+            _common = common;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public trans_track.Models.Vehicle Vehicle { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile file)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            if (file != null && file.Length > 0)
+                _common.UploadImage(Vehicle, file);
+            
+            _context.Vehicle.Add(Vehicle);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Vehicles/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using trans_track.Data;
+using trans_track.Models;
+
+namespace trans_track.Pages.Vehicle
+{
+    public class EditModel : PageModel
+    {
+        private readonly trans_track.Data.ApplicationDbContext _context;
+        private readonly ICommon _common;
+
+        public EditModel(trans_track.Data.ApplicationDbContext context, ICommon common)
+        {
+            _context = context;
+            _common = common;
+        }
+
+        [BindProperty]
+        public trans_track.Models.Vehicle Vehicle { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vehicle = await _context.Vehicle.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Vehicle == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile file)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Vehicle).State = EntityState.Modified;
+
+            try
+            {
+                if (file != null && file.Length > 0)
+                    _common.UploadImage(Vehicle, file);
+                
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehicleExists(Vehicle.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool VehicleExists(int id)
+        {
+            return _context.Vehicle.Any(e => e.ID == id);
+        }
+    }
+}
